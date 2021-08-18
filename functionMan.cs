4936fbc925b39b883b0ceb557cea8086943d67b9
perfect-ux <65482092+perfect-ux@users.noreply.github.com>
2021-08-18 12:20:19 +0200

    files and exe
    apk and exe

---
+++ functionMan.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class functionMan : MonoBehaviour
+{
+    public void BackMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
